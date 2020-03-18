a920a75eebdc18eb0dff10e9960c645fb9abf4d4
BrenoCardelle <43342547+BrenoCardelle@users.noreply.github.com>
2020-03-18 10:11:13 -0300

    Update fechadura.cpp

--- 2449/fechadura.cpp
+++ 2449/fechadura.cpp
@@ -6,35 +6,30 @@
 using namespace std;
 
 int main(){
-	int n = 0;
-	while(n<1000){
-		int pinos, altura;
-		cin >> pinos >> altura;
-		int vetor[pinos];
-		int i, j;
-		int cont = 0;
-		int aux = 0;
-		for(j=0; j<pinos; j++){
-			cin >> vetor[j];
-		}
-		for(i=0; i<pinos; i++){
-			if(vetor[i]<altura){
-				aux = altura - vetor[i];
-				vetor[i] = vetor[i] + aux;
-				cont = cont + aux;
-				vetor[i+1] += aux;
-			}
-			if(vetor[i]>altura){
-				aux = vetor[i] - altura;
-				vetor[i] = vetor[i] - aux;
-				cont = cont + aux;
-				vetor[i+1] -= aux;
-			}
-			aux = 0;
-		}
-		cout << cont;
-		n += 1;
+	int pinos, altura;
+	cin >> pinos >> altura;
+	int vetor[pinos];
+	int i, j;
+	int cont = 0;
+	int aux = 0;
+	for(j=0; j<pinos; j++){
+		cin >> vetor[j];
 	}
-	
+	for(i=0; i<pinos; i++){
+		if(vetor[i]<altura){
+			aux = altura - vetor[i];
+			vetor[i] = vetor[i] + aux;
+			cont = cont + aux;
+			vetor[i+1] += aux;
+		}
+		if(vetor[i]>altura){
+			aux = vetor[i] - altura;
+			vetor[i] = vetor[i] - aux;
+			cont = cont + aux;
+			vetor[i+1] -= aux;
+		}
+		aux = 0;
+	}
+	cout << cont << endl;	
 	return 0;
 }
